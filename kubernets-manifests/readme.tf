bbbfb001aa712179728036297f44bad3b40dbc66
Harichandana-Git-658 <awsproject658@gmail.com>
2026-01-21 14:05:57 +0530

    Update readme.tf

--- kubernets-manifests/readme.tf
+++ kubernets-manifests/readme.tf
@@ -4,3 +4,16 @@ Includes:
   Deploymentts
   Services
   Ingress
+
+What is Deployed:
+
+Frontend microservice
+Backend microservices
+Exposed using Kubernetes Service / Ingress
+Scalable and self-healing pods
+
+🔁 Deployment Strategy:
+
+Rolling updates
+Zero downtime deployments
+Managed by Argo CD (GitOps)
